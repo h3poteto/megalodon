70e959f68443892f9776d8530145455f1b9bac62
At-sushi <dkahw210@kyoto.zaq.ne.jp>
2018-12-31 01:41:08 +0900

    Optimized parser

--- src/parser.ts
+++ src/parser.ts
@@ -24,59 +24,46 @@ class Parser extends EventEmitter {
     this.message += chunk
     chunk = this.message
 
-    const size: number = chunk.length
-    let start: number = 0
-    let offset: number = 0
-    let curr: string | undefined
-    let next: string | undefined
+    let chunk_split: Array<string> = chunk.split('\n\n')
+    
+    for (var i = 0; i < chunk_split.length - 1; i++) {
+      const piece: string = chunk_split[i]
+      
+      if (!piece.length) continue // empty object
 
-    while (offset < size) {
-      curr = chunk[offset]
-      next = chunk[offset + 1]
+      const root: Array<string> = piece.split('\n')
 
-      if (curr === '\n' && next === '\n') {
-        const piece: string = chunk.slice(start, offset)
+      // should never happen, as long as mastodon doesn't change API messages
+      if (root.length !== 2) continue
 
-        offset += 2
-        start = offset
+      // remove event and data markers
+      const event: string = root[0].substr(7)
+      const data: string = root[1].substr(6)
 
-        if (!piece.length) continue // empty object
-
-        const root: Array<string> = piece.split('\n')
-
-        // should never happen, as long as mastodon doesn't change API messages
-        if (root.length !== 2) continue
-
-        // remove event and data markers
-        const event: string = root[0].substr(7)
-        const data: string = root[1].substr(6)
-
-        try {
-          const obj = JSON.parse(data)
-          switch (event) {
-            case 'update':
-              this.emit('update', obj as Status)
-              break
-            case 'notification':
-              this.emit('notification', obj as Notification)
-              break
-            case 'delete':
-              // When delete, data is an ID of the deleted status
-              this.emit('delete', obj as number)
-              break
-            default:
-              this.emit('error', new Error(`Unknown event has received: ${event}`))
-              break
-          }
-        } catch (err) {
-          this.emit('error', new Error(`Error parsing API reply: '${piece}', error message: '${err}'`))
-        } finally {
-          continue
+      try {
+        const obj = JSON.parse(data)
+        switch (event) {
+          case 'update':
+            this.emit('update', obj as Status)
+            break
+          case 'notification':
+            this.emit('notification', obj as Notification)
+            break
+          case 'delete':
+            // When delete, data is an ID of the deleted status
+            this.emit('delete', obj as number)
+            break
+          default:
+            this.emit('error', new Error(`Unknown event has received: ${event}`))
+            break
         }
+      } catch (err) {
+        this.emit('error', new Error(`Error parsing API reply: '${piece}', error message: '${err}'`))
+      } finally {
+        continue
       }
-      offset++
     }
-    this.message = chunk.slice(start, size)
+    this.message = chunk_split[chunk_split.length - 1]
   }
 }
 
