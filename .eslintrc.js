36bc42fdaac4de061a4b91f144e577cc98e8f2e6
AkiraFukushima <h3.poteto@gmail.com>
2019-05-25 22:10:10 +0900

    Setup eslint for typescript

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: ['standard', 'prettier'],
+  globals: {
+    __static: true
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_'
+      }
+    ],
+    camelcase: 'off',
+    '@typescript-eslint/camelcase': 'off',
+    'space-before-function-paren': 'off'
+  }
+}
