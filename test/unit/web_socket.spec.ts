dd1b9d6cb613f1976fc122bc6d111413884a8e43
AkiraFukushima <h3.poteto@gmail.com>
2019-08-24 00:17:10 +0900

    refs #53 Add unit tests for web_socket

---
+++ test/unit/web_socket.spec.ts
@@ -0,0 +1,179 @@
+import { Parser } from '@/web_socket'
+import { Account } from '@/entities/account'
+import { Status } from '@/entities/status'
+import { Application } from '@/entities/application'
+import { Notification } from '@/entities/notification'
+import { Conversation } from '@/entities/conversation'
+
+const account: Account = {
+  id: '1',
+  username: 'h3poteto',
+  acct: 'h3poteto@pleroma.io',
+  display_name: 'h3poteto',
+  locked: false,
+  created_at: '2019-03-26T21:30:32',
+  followers_count: 10,
+  following_count: 10,
+  statuses_count: 100,
+  note: 'engineer',
+  url: 'https://pleroma.io',
+  avatar: '',
+  avatar_static: '',
+  header: '',
+  header_static: '',
+  emojis: [],
+  moved: null,
+  fields: null,
+  bot: false
+}
+const status: Status = {
+  id: '1',
+  uri: 'http://example.com',
+  url: 'http://example.com',
+  account: account,
+  in_reply_to_id: null,
+  in_reply_to_account_id: null,
+  reblog: null,
+  content: 'hoge',
+  created_at: '2019-03-26T21:40:32',
+  emojis: [],
+  replies_count: 0,
+  reblogs_count: 0,
+  favourites_count: 0,
+  reblogged: null,
+  favourited: null,
+  muted: null,
+  sensitive: false,
+  spoiler_text: '',
+  visibility: 'public',
+  media_attachments: [],
+  mentions: [],
+  tags: [],
+  card: null,
+  poll: null,
+  application: {
+    name: 'Web'
+  } as Application,
+  language: null,
+  pinned: null
+}
+
+const notification: Notification = {
+  id: '1',
+  account: account,
+  status: status,
+  type: 'favourite',
+  created_at: '2019-04-01T17:01:32'
+}
+
+const conversation: Conversation = {
+  id: '1',
+  accounts: [account],
+  last_status: status,
+  unread: true
+}
+
+describe('parser', () => {
+  let parser: Parser
+  beforeEach(() => {
+    parser = new Parser()
+  })
+
+  describe('parse', () => {
+    describe('message is heartbeat', () => {
+      describe('message is an object', () => {
+        const message = Buffer.alloc(0)
+
+        it('should be called', () => {
+          const spy = jest.fn()
+          parser.once('heartbeat', spy)
+          parser.parse(message)
+          expect(spy).toHaveBeenCalledWith({})
+        })
+      })
+      describe('message is empty string', () => {
+        const message: string = ''
+
+        it('should be called', () => {
+          const spy = jest.fn()
+          parser.once('heartbeat', spy)
+          parser.parse(message)
+          expect(spy).toHaveBeenCalledWith({})
+        })
+      })
+    })
+
+    describe('message is not json', () => {
+      describe('event is delete', () => {
+        const message = JSON.stringify({
+          event: 'delete',
+          payload: '12asdf34'
+        })
+
+        it('should be called', () => {
+          const spy = jest.fn()
+          parser.once('delete', spy)
+          parser.parse(message)
+          expect(spy).toHaveBeenCalledWith('12asdf34')
+        })
+      })
+      describe('event is not delete', () => {
+        const message = JSON.stringify({
+          event: 'event',
+          payload: '12asdf34'
+        })
+
+        it('should be called', () => {
+          const error = jest.fn()
+          const deleted = jest.fn()
+          parser.once('error', error)
+          parser.once('delete', deleted)
+          parser.parse(message)
+          expect(error).toHaveBeenCalled()
+          expect(deleted).not.toHaveBeenCalled()
+        })
+      })
+    })
+
+    describe('message is json', () => {
+      describe('event is update', () => {
+        const message = JSON.stringify({
+          event: 'update',
+          payload: JSON.stringify(status)
+        })
+        it('should be called', () => {
+          const spy = jest.fn()
+          parser.once('update', spy)
+          parser.parse(message)
+          expect(spy).toHaveBeenCalledWith(status)
+        })
+      })
+
+      describe('event is notification', () => {
+        const message = JSON.stringify({
+          event: 'notification',
+          payload: JSON.stringify(notification)
+        })
+        it('should be called', () => {
+          const spy = jest.fn()
+          parser.once('notification', spy)
+          parser.parse(message)
+          expect(spy).toHaveBeenCalledWith(notification)
+        })
+      })
+
+      describe('event is conversation', () => {
+        const message = JSON.stringify({
+          event: 'conversation',
+          payload: JSON.stringify(conversation)
+        })
+        it('should be called', () => {
+          const spy = jest.fn()
+          parser.once('conversation', spy)
+          parser.parse(message)
+          expect(spy).toHaveBeenCalledWith(conversation)
+        })
+      })
+    })
+  })
+})
