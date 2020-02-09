e0f99fe322e48d06d40964b43925ea652494a745
AkiraFukushima <h3.poteto@gmail.com>
2020-02-10 01:12:00 +0900

    Fix entity for browser example

--- example/browser/src/index.ts
+++ example/browser/src/index.ts
@@ -1,10 +1,10 @@
-import { Mastodon, Instance, Response } from 'megalodon'
+import { Mastodon, Entity, Response } from 'megalodon'
 
 const BASE_URL: string = 'http://mastodon.social'
 console.log('start')
 
 const client = new Mastodon(BASE_URL)
 
-client.getInstance().then((res: Response<Instance>) => {
+client.getInstance().then((res: Response<Entity.Instance>) => {
   console.log(res)
 })
