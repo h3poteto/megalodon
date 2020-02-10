964875bfef793d5b8d9242a27097296d2d770b7d
AkiraFukushima <h3.poteto@gmail.com>
2020-02-11 00:16:29 +0900

    Use generator in browser example

--- example/browser/src/index.ts
+++ example/browser/src/index.ts
@@ -1,9 +1,9 @@
-import { Mastodon, Entity, Response } from 'megalodon'
+import generator, { Entity, Response } from 'megalodon'
 
 const BASE_URL: string = 'http://mastodon.social'
 console.log('start')
 
-const client = new Mastodon(BASE_URL)
+const client = generator('mastodon', BASE_URL)
 
 client.getInstance().then((res: Response<Entity.Instance>) => {
   console.log(res)
