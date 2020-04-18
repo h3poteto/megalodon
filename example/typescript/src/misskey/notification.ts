16f58968218e52475c898eb999f12f52dc23d252
AkiraFukushima <h3.poteto@gmail.com>
2020-04-18 18:00:38 +0900

    refs #335 Add example for emoji reactions in notifications

--- example/typescript/src/misskey/notification.ts
+++ example/typescript/src/misskey/notification.ts
@@ -2,14 +2,14 @@ import generator, { NotificationType } from 'megalodon'
 
 declare var process: {
   env: {
-    PLEROMA_ACCESS_TOKEN: string
+    MISSKEY_ACCESS_TOKEN: string
   }
 }
 
-const BASE_URL: string = 'https://pleroma.io'
+const BASE_URL: string = 'https://misskey.io'
 
-const access_token: string = process.env.PLEROMA_ACCESS_TOKEN
+const access_token: string = process.env.MISSKEY_ACCESS_TOKEN
 
-const client = generator('pleroma', BASE_URL, access_token)
+const client = generator('misskey', BASE_URL, access_token)
 
 client.getNotifications({ exclude_types: [NotificationType.Favourite, NotificationType.Reblog] }).then(res => console.log(res.data))
