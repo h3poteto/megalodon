8d9b4a75cbb0470bcf7040784980fe1abb171c8e
AkiraFukushima <h3.poteto@gmail.com>
2022-07-10 15:26:24 +0900

    refs #1292 Add configurations block in instance for new mastodon API

--- megalodon/src/pleroma/entities/instance.ts
+++ megalodon/src/pleroma/entities/instance.ts
@@ -16,5 +16,26 @@ namespace PleromaEntity {
     contact_account: Account | null
     max_toot_chars?: number
     registrations?: boolean
+    configuration?: {
+      statuses: {
+        max_characters: number
+        max_media_attachments: number
+        characters_reserved_per_url: number
+      }
+      media_attachments: {
+        supported_mime_types: Array<string>
+        image_size_limit: number
+        image_matrix_limit: number
+        video_size_limit: number
+        video_frame_limit: number
+        video_matrix_limit: number
+      }
+      polls: {
+        max_options: number
+        max_characters_per_option: number
+        min_expiration: number
+        max_expiration: number
+      }
+    }
   }
 }
