454846ab58d6099fcf57589e0252debef6a77543
AkiraFukushima <h3.poteto@gmail.com>
2022-11-23 10:34:21 +0900

    refs #1460 Fix pleroma marker entity

--- megalodon/src/pleroma/entities/marker.ts
+++ megalodon/src/pleroma/entities/marker.ts
@@ -1,14 +1,12 @@
 namespace PleromaEntity {
   export type Marker = {
-    home: {
-      last_read_id: string
-      version: number
-      updated_at: string
-    }
     notifications: {
       last_read_id: string
       version: number
       updated_at: string
+      pleroma: {
+        unread_count: number
+      }
     }
   }
 }
