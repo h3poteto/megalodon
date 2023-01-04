862ed06cb947443c65ebfdedc350e9048b7d4953
AkiraFukushima <h3.poteto@gmail.com>
2023-01-05 00:26:11 +0900

    Use field in source entity

--- megalodon/src/mastodon/entities/source.ts
+++ megalodon/src/mastodon/entities/source.ts
@@ -1,9 +1,10 @@
+/// <reference path="field.ts" />
 namespace MastodonEntity {
   export type Source = {
     privacy: string | null
     sensitive: boolean | null
     language: string | null
     note: string
-    fields: object
+    fields: Array<Field>
   }
 }
