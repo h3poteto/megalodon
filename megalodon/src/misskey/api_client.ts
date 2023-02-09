499090039075f0b96db006b83a54fba90f70233d
cutls <web-pro@cutls.com>
2023-02-09 19:36:14 +0900

    host in this server

--- megalodon/src/misskey/api_client.ts
+++ megalodon/src/misskey/api_client.ts
@@ -51,6 +51,7 @@ namespace MisskeyAPI {
     }
 
     export const user = (u: Entity.User): MegalodonEntity.Account => {
+      host = host.replace('https://', '')
       let acct = u.username
       let acctUrl = `https://${u.host || 'example.com'}/@${u.username}`
       if (u.host) {
