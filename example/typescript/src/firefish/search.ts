dc682ad34bc672fdb257ff714a6e1cc6320e0b76
AkiraFukushima <h3.poteto@gmail.com>
2023-09-28 00:14:43 +0900

    Ignore errors in Firefish search

---
+++ example/typescript/src/firefish/search.ts
@@ -0,0 +1,11 @@
+import generator, { MegalodonInterface } from 'megalodon'
+
+const BASE_URL: string = process.env.FIREFISH_URL!
+const access_token: string = process.env.FIREFISH_ACCESS_TOKEN!
+
+const client: MegalodonInterface = generator('firefish', BASE_URL, access_token)
+
+client
+  .search('h3poteto')
+  .then(res => console.log(res.data))
+  .catch(err => console.error(err))
