72f84d5eb83aa657ae9806605ca8a03e1efc8e8e
ThatOneCalculator <kainoa@t1c.dev>
2023-10-07 17:20:42 -0700

    add node polyfills for vite config

--- example/browser/vite.config.js
+++ example/browser/vite.config.js
@@ -3,6 +3,7 @@ import path from 'path';
 import envCompatible from 'vite-plugin-env-compatible';
 import { createHtmlPlugin } from 'vite-plugin-html';
 import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -38,14 +39,12 @@ export default defineConfig({
       }
     })
   ],
-  build: {}
-  // THE FOLLOWING IS NOT SUPPORTED BY VITE AND COPIED FROM THE OLD WEBPACK CONFIG
-  // May no longer be relevant: https://vitejs.dev/guide/philosophy.html#pushing-the-modern-web
-  // https-proxy-agent and socks-proxy-agent is node library, so can't compile for browser.
-  // So replace net, tls and dns which are node libraries.
-  // node: {
-  //   net: 'empty',
-  //   tls: 'empty',
-  //   dns: 'empty'
-  // }
+  build: {
+    plugins: [
+      nodePolyfills({
+         // https-proxy-agent and socks-proxy-agent is node library, so can't compile for browser.
+        include: ['net', 'tls', 'dns']
+      })
+    ]
+  }
 })
