f3f05be59907658eca0c74c0b917c5c72365a2c9
AkiraFukushima <h3.poteto@gmail.com>
2023-10-16 22:03:31 +0900

    Use getPreferences instead of getDefaultPostVisibility

--- megalodon/src/megalodon.ts
+++ megalodon/src/megalodon.ts
@@ -602,12 +602,6 @@ export interface MegalodonInterface {
    */
   getPreferences(): Promise<Response<Entity.Preferences>>
 
-  /**
-   * Get the default post privacy defined by the user in their account settings. Firefish only.
-   *
-   * @return Preferences.
-   */
-  getDefaultPostPrivacy(): Promise<Entity.Preferences['posting:default:visibility']>
   // ======================================
   // accounts/followed_tags
   // ======================================
