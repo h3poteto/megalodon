7e2fb4a845dd037839034d1f639b6daa5a5c848c
AkiraFukushima <h3.poteto@gmail.com>
2026-01-28 00:27:33 +0900

    Update example

--- example/node-cjs/src/mastodon/instance.ts
+++ example/node-cjs/src/mastodon/instance.ts
@@ -1,8 +1,10 @@
 import generator, { Entity, Response } from 'megalodon'
+import axios from 'axios'
 
 const BASE_URL: string = process.env.MASTODON_URL!
+const instance = axios.create()
 
-const client = generator('mastodon', BASE_URL)
+const client = generator('mastodon', BASE_URL, null, null, instance)
 
 client.getInstance().then((res: Response<Entity.Instance>) => {
   console.log(res.data)
