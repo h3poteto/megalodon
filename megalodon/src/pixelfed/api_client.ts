f4d09e82ee17e355507ae6b399ff14b07d17b356
AkiraFukushima <h3.poteto@gmail.com>
2026-02-03 23:44:18 +0900

    Set unknown to the default quote approval

--- megalodon/src/pixelfed/api_client.ts
+++ megalodon/src/pixelfed/api_client.ts
@@ -640,9 +640,9 @@ namespace PixelfedAPI {
       bookmarked: s.bookmarked ? s.bookmarked : false,
       quote: null,
       quote_approval: {
-        automatic: ['unsupported_policy'],
+        automatic: [],
         manual: [],
-        current_user: 'automatic'
+        current_user: 'unknown'
       }
     })
     export const status_params = (s: Entity.StatusParams): MegalodonEntity.StatusParams => s
